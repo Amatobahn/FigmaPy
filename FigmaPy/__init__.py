7517bb151fe8f1575f7be8fd99323cbb81106937
Greg Amato <amatobahn@gmail.com>
2018-05-16 12:35:47 -0500

    - updated wheel with proper setup - created init file for module

---
+++ FigmaPy/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+"""
+Figma API Wrapper
+~~~~~~~~~~~~~~~~~~~
+
+A basic wrapper for the Figma API.
+
+:copyright: (c) 2018 Greg Amato
+:license: Apache V2.0, see LICENSE for more details.
+
+"""
+
+__title__ = 'FigmaPy'
+__author__ = 'Greg Amato'
+__license__ = 'Apache V2.0'
+__copyright__ = 'Copyright 2018 Greg Amato'
+__version__ = '2018.1.0'
+
+from .figmapy import *
