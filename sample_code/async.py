27b91929196bf2370058b1d39b1a76a6f5e8b007
hannes <hannesdelbeke@gmail.com>
2022-08-04 14:09:57 -0700

    async sample, mention issues

--- sample_code/async.py
+++ sample_code/async.py
@@ -6,6 +6,9 @@ import time
 file_key = 'REDACTED'
 auth_key = 'REDACTED'
 
+# TODO create a better sample, this actually results in the same times
+#  and the async version sometimes crashes randomly with a no key find in dict error
+
 figmaPy = FigmaPy.FigmaPy(token=auth_key)
 file = figmaPy.get_file(file_key)
 page1 = file.document.children[0]
@@ -23,3 +26,7 @@ loop = asyncio.get_event_loop()
 result = loop.run_until_complete(figmaPy.get_file_images(file_key=file_key, ids=node_ids))
 print(time.time() - start)
 print(result.images)
+
+figmapy_async.client.close()
+# TODO fix this error RuntimeWarning: coroutine 'ClientSession.close' was never awaited
+#   figma_session_async.client.close()
