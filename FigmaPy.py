bc71b90a888b1ae07805efcade303af1243af3a0
hannes <hannesdelbeke@gmail.com>
2022-08-16 14:07:19 -0700

    backward compatible

---
+++ FigmaPy.py
@@ -0,0 +1,5 @@
+# Only include this file in your project to handle backwards compatibility
+# to support any old projects that still use `import FigmaPy` (camelCase)
+# instead of `import figmapy` (lowercase)
+
+from figmapy import *
